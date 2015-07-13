e6797c4391a9ad5fb3d5958f052c2367b5f09599
Suzanne Wang <suzanneswang@gmail.com>
2015-07-13 12:30:24 -0700

    basic scaffolding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,25 @@
 'use strict';
 
+//**** main module ****//
+
+var poliView = angular.module('poliView', []);
+
+poliView.config(function($routeProvider) {
+  $routeProvider
+    .when('/' {
+      templateUrl: '../views/home.html',
+      controller: ''
+    })
+})
+
+var Influence = require('influence');
+var influence = new Influence(apiKey);
+console.log('test');
+influence.entityOverview('97737bb56b6a4211bcc57a837368b1a4', null, function(err, json) {
+  if (err) throw err;
+  console.log(json);
+});
+
 /**
  * @ngdoc overview
  * @name mvpApp
@@ -8,29 +28,30 @@
  *
  * Main module of the application.
  */
-angular
-  .module('mvpApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+// angular
+//   .module('mvpApp', [
+//     'ngAnimate',
+//     'ngCookies',
+//     'ngMessages',
+//     'ngResource',
+//     'ngRoute',
+//     'ngSanitize',
+//     'ngTouch'
+//   ])
+//   .config(function ($routeProvider) {
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: 'views/main.html',
+//         controller: 'MainCtrl',
+//         controllerAs: 'main'
+//       })
+//       .when('/about', {
+//         templateUrl: 'views/about.html',
+//         controller: 'AboutCtrl',
+//         controllerAs: 'about'
+//       })
+//       .otherwise({
+//         redirectTo: '/'
+//       });
+//   });
+
