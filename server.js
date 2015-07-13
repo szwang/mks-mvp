403193ced3444e08541f4be683c1c534bfbc9153
Suzanne Wang <suzanneswang@gmail.com>
2015-07-13 16:48:51 -0700

    server up

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var partials = require('express-partials');
-var jade = require('jade');
 var path = require('path');
 
 var port = process.env.PORT || 4568;
 
 var app = express();
 
-var Influence = require('sunlight-influence');
-
 app.listen(port);
 console.log('Server now listening on port ' + port);
 
-app.set('view engine', 'jade');
-
-app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname, '/app/views', 'main.html'));
-})
+app.use(express.static('app'));
